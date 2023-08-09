c06f29a9f7ed1780438a2dfebded7f9e7a0b2b0e
yuvaraj0705 <113332600+yuvaraj0705@users.noreply.github.com>
2023-08-09 12:46:45 +0530

    Add files via upload

---
+++ rotate180.py
@@ -0,0 +1,21 @@
+import cv2
+
+# Load the image
+image_path = 'C:/Users/ASUS/OneDrive/Documents/COMPUTER VISION/images/musk.jpg'
+img = cv2.imread(image_path)
+
+# Check if the image was loaded properly
+if img is None:
+    print("Error: Image not loaded properly.")
+else:
+    # Define the rotation angle in degrees
+    angle = 45
+
+    # Rotate the image
+    rotated_img = cv2.rotate(img, cv2.ROTATE_180)
+
+    # Display the original and rotated images side by side
+    cv2.imshow('Original', img)
+    cv2.imshow('rotated', rotated_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
