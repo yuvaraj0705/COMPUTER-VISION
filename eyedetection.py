5c7b8c0e74ff67df7a285b91ac86ef4def779d10
yuvaraj0705 <113332600+yuvaraj0705@users.noreply.github.com>
2023-08-10 10:11:23 +0530

    Add files via upload

---
+++ eyedetection.py
@@ -0,0 +1,10 @@
+import cv2
+img = cv2.imread('C:/Users/ASUS/OneDrive/Documents/COMPUTER VISION/images/messi.jpg')
+gray_img = cv2.cvtColor(img,cv2.IMREAD_GRAYSCALE)
+haarcascade =cv2.CascadeClassifier("C:\\Users\\ASUS\\OneDrive\\Documents\\COMPUTER VISION\\images\\haarcascade_eye.xml")
+faces_rec = haarcascade.detectMultiScale(gray_img,1.07,8)
+for (x,y,w,h) in faces_rec:
+    cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0), 2)
+    cv2.imshow('Detected eye',img)
+
+    cv2.waitKey(0)
